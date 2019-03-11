4cab02f9e1d9439740bbdab5b2285594984e3f58
Chavez102 <43897985+Chavez102@users.noreply.github.com>
2019-03-10 20:20:46 -0400

    First Homework uploaded

---
+++ HW1 Classes Objects  and Bags/src/problem_1/Demo.java
@@ -0,0 +1,14 @@
+package problem_1;
+
+import java.io.FileNotFoundException;
+
+public class Demo {
+
+	public static void main(String[] args) throws FileNotFoundException 
+	{
+		Name_Warehouse_class.createFullNames();
+		Name_Warehouse_class.display();
+		
+	}
+
+}
